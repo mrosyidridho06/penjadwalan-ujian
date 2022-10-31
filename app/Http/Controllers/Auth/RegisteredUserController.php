2d7b0374ff578059dd0eb4c1f4e5c4247a9a3a5b
mrosyidridho06 <61150937+mrosyidridho06@users.noreply.github.com>
2022-10-31 15:09:14 +0800

    update unique nim dan forgot pass

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,7 +44,7 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'nim' => ['required', 'digits:10', 'numeric'],
+            'nim' => ['required', 'digits:10', 'numeric', 'unique:mahasiswas'],
             'angkatan' => ['required'],
             'alamat' => ['required', 'string'],
             'prodi' => ['required'],
--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/" class="d-flex justify-content-center mb-4">
@@ -33,4 +33,50 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+@include('layouts.partials.head')
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <div id="app">
+            <section class="section">
+                <div class="container mt-5">
+                    <div class="row">
+                        <div
+                            class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                            <div class="login-brand">
+                                <img src="{{ asset('images/unmul.png') }}" alt="logo" width="100" class="shadow-light rounded-circle">
+                            </div>
+
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h4>Reset Password</h4>
+                                </div>
+
+                                <div class="card-body">
+                                    <p class="text-muted">We will send a link to reset your password</p>
+                                    <form method="POST" action="{{ route('password.email') }}">
+                                        @csrf
+                                        <!-- Email Address -->
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input id="email" type="email" class="form-control" name="email" :value="old('email')" tabindex="1" required autofocus>
+                                        </div>
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                                Reset Password
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="simple-footer">
+                                Copyright &copy; Farmasi 2022
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
