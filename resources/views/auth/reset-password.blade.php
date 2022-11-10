44a7d13cb3d5d8d2dd8dae3097b8965bb2a9579f
mrosyidridho06 <61150937+mrosyidridho06@users.noreply.github.com>
2022-11-10 14:23:03 +0800

    update view password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/" class="d-flex justify-content-center mb-4">
@@ -45,4 +45,69 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+@include('layouts.partials.head')
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <div id="app">
+            <section class="section">
+                <div class="container mt-5">
+                    <div class="row">
+                        <div
+                            class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                            <div class="login-brand">
+                                <a href="/">
+                                    <img src="{{ asset('images/unmul.png') }}" alt="logo" width="100" class="shadow-light rounded-circle">
+                                </a>
+                            </div>
+
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h4>Reset Password</h4>
+                                </div>
+                                @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+                                <div class="card-body">
+                                    <form method="POST" action="{{ route('password.update') }}">
+                                        @csrf
+                                        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input id="email" type="email" class="form-control" name="email" :value="old('email', $request->email)" tabindex="1" required autofocus>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="email">Password</label>
+                                            <input id="email" type="password" class="form-control" name="password" tabindex="1" required autofocus>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="email">Confirm Password</label>
+                                            <input id="email" type="password" class="form-control" name="password_confirmation" tabindex="1" required autofocus>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                                Reset Password
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="simple-footer">
+                                Copyright &copy; Farmasi Unmul 2022
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
