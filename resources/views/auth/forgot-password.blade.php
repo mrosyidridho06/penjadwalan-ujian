9f02343c3428e4543158ee21408670bdceb278e4
mrosyidridho06 <61150937+mrosyidridho06@users.noreply.github.com>
2022-11-10 14:15:27 +0800

    update view reset

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -41,16 +41,6 @@
         <div id="app">
             <section class="section">
                 <div class="container mt-5">
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    <x-auth-session-status class="mb-4" :status="session('status')" />
                     <div class="row">
                         <div
                             class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
@@ -64,7 +54,20 @@
                                 <div class="card-header">
                                     <h4>Reset Password</h4>
                                 </div>
-
+                                @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+                                @if (session('status'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
                                 <div class="card-body">
                                     <p class="text-muted">We will send a link to reset your password</p>
                                     <form method="POST" action="{{ route('password.email') }}">
